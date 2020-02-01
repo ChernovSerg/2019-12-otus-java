87b562ce31789d329f7df751fee6df11e832eec9
Sergey Chernov <chernov_serg@mail.ru>
2020-02-02 01:19:55 +0300

    Результаты экспериментов с GC в файле "Вывод по GC.xlsx"

---
+++ hw-experiences-gc/src/main/java/ru/otus/chernovsa/gc/ExpGc.java
@@ -0,0 +1,39 @@
+package ru.otus.chernovsa.gc;
+
+import java.util.HashSet;
+
+public class ExpGc implements ExpGcMBean {
+    private final int loopCounter;
+    private volatile int size = 0;
+    private HashSet<Integer> integerHashSet;
+
+    public ExpGc(int loopCounter) {
+        this.loopCounter = loopCounter;
+    }
+
+    @Override
+    public int getSize() {
+        return 0;
+    }
+
+    @Override
+    public void setSize(int size) {
+        System.out.println( "new size:" + size );
+        this.size = size;
+        integerHashSet = new HashSet<>(this.size);
+    }
+
+    void run() throws InterruptedException {
+        for ( int idx = 0; idx < loopCounter; idx++ ) {
+            int local = size;
+            Object[] array = new Object[ local ];
+            for ( int i = 0; i < local; i++ ) {
+                array[ i ] = new String( new char[ 0 ] );
+            }
+//            Thread.sleep( 100 ); //Label_1
+        }
+        for (int i = 0; i < this.size; i++) {
+            integerHashSet.add(Integer.valueOf(i));
+        }
+    }
+}
